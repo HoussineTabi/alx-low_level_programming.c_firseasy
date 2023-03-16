167a37abed3399ddada25ab2e8a9a77498cd38ef
elhoussien <0620036304he@gmail.com>
2023-03-16 07:32:28 -0700

    some corre

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /*
  * main() starting the programme 
  * retun 0 always
- * */
+ */
 int main(void)
 {
-   puts("Programming is like building a multilingual puzzle");
-   return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
