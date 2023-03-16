8352c2c582ab618125341ea21ca226b98374840a
elhoussien <0620036304he@gmail.com>
2023-03-16 10:19:26 -0700

    new try

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /*
  * the main start the programme
  *
- * Descriptin :  printf("with proper grammar, but the outcome is a piece of art,\n")
+ * Descriptin :  print string 
  *
  * Return 0 Always
  */
-int main(void)
+int main(void) /*Description : print string on the string*/
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
