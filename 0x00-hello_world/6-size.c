f3de93a841166f08073498cf0a8314b4b6cdfe80
elhoussien <0620036304he@gmail.com>
2023-03-16 11:25:55 -0700

    size of a an

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 /*Description : this programme disply the size of various types*/
 int main(void)
 {
-printf("Size of char is %zu byte(s)\n", sizeof(char));
-printf("Size of int is %zu byte(s)\n", sizeof(int));
-printf("Size of long int  is %zu byte(s)\n", sizeof(long int));
-printf("Size of long long int  is %zu byte(s)\n", sizeof(long long int));
-printf("Size  of float  is %zu byte(s)\n", sizeof(float));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size  of float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
