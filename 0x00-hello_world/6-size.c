0d6bb9357412aa5d4ad9c8639cadbbdafedf3510
elhoussien <0620036304he@gmail.com>
2023-03-16 11:07:21 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*Description : this programme disply the size of various types*/
+int main(void)
+{
+printf("The size of char is %zu bytes\n", sizeof(char));
+printf("The size of short is %zu bytes\n", sizeof(short));
+printf("The size of int is %zu bytes\n", sizeof(int));
+printf("The size of long is %zu bytes\n", sizeof(long));
+printf("The size of float is %zu bytes\n", sizeof(float));
+printf("The size of double is %zu bytes\n", sizeof(double));
+printf("The size of long double is %zu bytes\n", sizeof(long double));
+return (0);
+}
