823b5b4c750b5c506fdb5c3f6805f36094d41198
elhoussien <0620036304he@gmail.com>
2023-03-16 07:23:11 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * main() starting the programme 
+ * retun 0 always
+ * */
+int main(void)
+{
+   puts("Programming is like building a multilinuagl puzzle");
+   return 0;
+}
