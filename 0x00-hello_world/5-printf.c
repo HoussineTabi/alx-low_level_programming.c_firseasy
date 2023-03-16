765e21bdf5b1ab622e2d694c281b2d07f3be333f
elhoussien <0620036304he@gmail.com>
2023-03-16 10:32:31 -0700

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  *
  * Return 0 Always
  */
-int main(void)//Description this main print string
+int main(void)/*Description this main print string*/
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
