a59491c30ff7bc8e4876bd17e8f926406fde5b39
elhoussien <0620036304he@gmail.com>
2023-03-16 11:19:29 -0700

    correction 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 /*Description : this programme disply the size of various types*/
 int main(void)
 {
-printf("The size of char is %zu bytes\n", sizeof(char));
-printf("The size of int is %zu bytes\n", sizeof(int));
-printf("The size of long is %zu bytes\n", sizeof(long int));
-printf("The size of float is %zu bytes\n", sizeof(long long int));
-printf("The size of double is %zu bytes\n", sizeof(float));
+printf("Size of char is %zu byte(s)\n", sizeof(char));
+printf("Size of int is %zu byte(s)\n", sizeof(int));
+printf("Size of long int  is %zu byte(s)\n", sizeof(long int));
+printf("Size of long long int  is %zu byte(s)\n", sizeof(long long int));
+printf("Size  of float  is %zu byte(s)\n", sizeof(float));
 return (0);
 }
