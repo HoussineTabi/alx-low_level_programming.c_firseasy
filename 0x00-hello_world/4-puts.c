cdc346d711b4302b42ba70f746b7e446bcb9d442
elhoussien <0620036304he@gmail.com>
2023-03-16 08:04:17 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
  * main : starting the programme
  * retun 0 always
  */
-int main(void) /*this function main is going to print a string in the standard output*/
+int main(void)
 {
+/*this function main is going to print a string in the standard output*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
