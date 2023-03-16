64e8b6eb804f9b870600a819f2945e2849d46c68
elhoussien <0620036304he@gmail.com>
2023-03-16 10:23:10 -0700

    corre

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /*
  * the main start the programme
  *
- * Descriptin :  print string 
+ * Descriptin:  print string
  *
  * Return 0 Always
  */
-int main(void) /*Description : print string on the string*/
+int main(void)/* Description: print string on the string*/
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
