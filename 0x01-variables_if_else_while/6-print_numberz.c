63b6c34d15df57f44a4ce6c7ff85e60f27d9ae41
elhoussien <0620036304he@gmail.com>
2023-03-17 08:48:38 -0700

    taks 6 is done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - This programme print numbers from 0 to 10
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	/*blanck line after declaration */
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
