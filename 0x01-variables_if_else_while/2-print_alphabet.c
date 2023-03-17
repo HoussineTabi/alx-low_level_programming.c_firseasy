774421efebe659337e6776614707a006f206fc45
elhoussien <0620036304he@gmail.com>
2023-03-17 07:26:20 -0700

    task 2 done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Print alphanets in lower case
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
