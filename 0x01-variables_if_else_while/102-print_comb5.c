479db101f53345eb0355b549e917bc0015a5a300
elhoussien <0620036304he@gmail.com>
2023-03-18 14:04:25 -0700

    final task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+/**
+ * main - This task for combinition long
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			k = i;
+			while (k <= 9)
+			{
+				l = j + 1;
+				while (l <= 9)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i != 9 || j != 8 || k != 9 || l != 9)
+					{
+						putchar(',');
+					}
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
