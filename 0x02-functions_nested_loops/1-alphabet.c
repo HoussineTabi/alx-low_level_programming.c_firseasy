e6c36ab0e06d4c6bf726a2f88fa13ae16f2f248c
elhoussien <0620036304he@gmail.com>
2023-03-21 03:50:29 -0700

    correction file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,27 +1,24 @@
 #include "main.h"
-
-/**
- * main - This programme is for write alphabets in lowercase
- * We are going to use function to do that
- * Return: 0 (Success)
- */
-
 void print_alphabet(void);
+/**
+ * print_alphabet -  printing lower alphabet starting from a to z
+ */
+	void print_alphabet(void)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+		putchar('\n');
+	}
+/**
+ * main - tha function princpal
+ *
+ * Return: The value that return when the execution is end
+ */
 
 int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-
-/**
- * print_alphabet -  printing lower alphabet starting from a to z
- */
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-	putchar('\n');
 }
