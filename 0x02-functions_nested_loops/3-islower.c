085ba26a246c873ddfbfb1aae99690ed1febdf36
elhoussien <0620036304he@gmail.com>
2023-03-21 18:21:16 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+/**
+ * _islower - Tell us if a charracter is in lower case
+ *
+ * @i: is an integer parameter
+ * Return: 1 is true and 0 otherwise
+ */
+int _islower(int i)
+{
+	if (i >= 98 && i <= 118)
+		return (1);
+	return 0;
+
+}
