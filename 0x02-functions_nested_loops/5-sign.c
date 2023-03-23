095c77b5729cf2e85e7b8c71194a11b5302cd78d
elhoussien <0620036304he@gmail.com>
2023-03-22 17:54:49 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,17 +12,17 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		_putchar(45 + '0');
+		_putchar(45);
 		return (-1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(0);
 		return (0);
 	}
 	else
 	{
-		putchar(43 + '0');
+		putchar(43);
 		return (1);
 	}
 }
