e52ed4edeaaa96680023f5787a12285184eccc15
elhoussien <0620036304he@gmail.com>
2023-03-23 15:09:30 -0700

    square is done

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,13 +9,13 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (n <= 0)
+	if (size <= 0)
 		_putchar('\n');
 	else
 	{
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j <= n; j++)
+		for (j = 1; j <= size; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
