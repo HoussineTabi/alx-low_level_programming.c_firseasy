f42485f357c82f308186cda901ae88a35cf6c904
elhoussien <0620036304he@gmail.com>
2023-03-22 17:28:04 -0700

    taks number five sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - check the sign of an int
+ * @n: parameter of type int
+ * Return: 1 and print + if the number is positive
+ * Return: 0 and print 0 if the number is 0
+ * Return: -1 and print - if the number is negative
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('+');
+		return (1);
+	}
+}
