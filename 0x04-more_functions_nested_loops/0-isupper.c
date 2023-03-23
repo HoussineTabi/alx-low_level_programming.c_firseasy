4b58047db91a629653980fc32829f77b2a4bf489
elhoussien <0620036304he@gmail.com>
2023-03-23 03:20:23 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,11 @@
 
 /**
  * _isupper - check if an alphabet is in upper case
- * @c: parameter of type charracter
+ * @c: parameter of type integer
  * Return: 1 if c is upper or 0 otherwie
  */
 
-int _isupper(char c)
+int _isupper(int c)
 {
 	if ((c >= 65) && (c < 93))
 		return (1);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-int _isupper(char);
+int _isupper(int);
 int -putchar(char);
 
 #endif /* MAIN_h */
