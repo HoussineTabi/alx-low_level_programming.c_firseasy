66589146d13fd1681028103124954b2b12f99fc9
elhoussien <0620036304he@gmail.com>
2023-03-23 15:02:47 -0700

    add the if else condition

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,10 +9,15 @@ void print_square(int size)
 {
 	int i, j;
 
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
 	for (i = 0; i <= size; i++)
 	{
 		for (j = 0; j <= n; j++)
 			_putchar('#');
 		_putchar('\n');
+	}
 	}
 }
