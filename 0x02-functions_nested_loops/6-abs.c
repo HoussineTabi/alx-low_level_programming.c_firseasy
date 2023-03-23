66036ab4e9f5cf032238cd6366434e5e5632560b
elhoussien <0620036304he@gmail.com>
2023-03-22 18:49:15 -0700

    absolut integer value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - return the value absolute of an integer
+ * @a: an interger parameter
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (a * -1);
+	else
+		return (a);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,9 @@
 
 int print_sign(int n);
 int _isalpha(int c);
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int print_sign(int n);
+int _abs(int);
