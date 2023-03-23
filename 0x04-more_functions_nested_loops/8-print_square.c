77fa6e742d883ed2bf52455e80e85e419420ccc7
elhoussien <0620036304he@gmail.com>
2023-03-23 15:00:15 -0700

    task square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_square - square is a function print sqaure on thescreen
+ * @size: is an integer parameter
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i <= size; i++)
+	{
+		for (j = 0; j <= n; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
