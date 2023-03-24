5e05b1e7cf3b1271151add8b41c267fc1a13ff3a
elhoussien <0620036304he@gmail.com>
2023-03-23 18:21:15 -0700

    task 101 done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,11 +7,26 @@
  */
 void print_number(int n)
 {
-	if (n / 10 != 0)
+	if (n >= 0)
 	{
-		print_number(n / 10);
-		_putchar(n % 10 + '0');
+		if (n / 10 != 0)
+		{
+			print_number(n / 10);
+			_putchar(n % 10 + '0');
+		}
+		else
+			_putchar(n + '0');
 	}
-	else 
-		_putchar(n + '0');
+	else
+	{
+		n = n * -1;
+		_putchar('-');
+		if (n / 10 != 0)
+		{
+			print_number(n / 10);
+			_putchar(n % 10 + '0');
+		}
+		else
+			_putchar(n + '0');
+	}
 }
