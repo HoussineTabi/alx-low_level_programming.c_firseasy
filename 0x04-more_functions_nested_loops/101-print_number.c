ed6b1591075fe26c3cb309e76ac2901161877446
elhoussien <0620036304he@gmail.com>
2023-03-23 18:09:14 -0700

    task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - printing a number using _putchar
+ *
+ * @n: integer parameter
+ */
+void print_number(int n)
+{
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
+	else 
+		_putchar(n + '0');
+}
