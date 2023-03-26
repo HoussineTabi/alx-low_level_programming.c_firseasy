fb97872d5df4a14aeed670e7a7d3adf0f9c53c2b
elhoussien <0620036304he@gmail.com>
2023-03-26 06:38:45 -0700

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_to_98 - this function print numbers from the enter number to 98
+ *
+ * @a: ineger parameter
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+				printf("%d", i);
+			else
+				printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = n; i <= 98; i--)
+		{
+			if (i == 98)
+				printf("%d", i);
+			else
+				printf("%d, ", i);
+		}
+		printf("\n");
+	}
+}
