943b567e04fa5367c3766a10d59dc040b2c06dce
elhoussien <0620036304he@gmail.com>
2023-03-26 08:54:15 -0700

    100_task_is_done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,24 +14,19 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				a = i * j;
+				if (j != 0)
+					_putchar(' ');
 				if (a >= 100)
 				{
-					_putchar(' ');
 					_putchar(a / 100 + '0');
-					a = a % 100;
-					_putchar(a / 10 + '0');
-					_putchar(a % 10 + '0');
-					if (j != n)
-						_putchar(',');
+					_putchar((a % 100) / 10 + '0');
+					_putchar((a % 100) % 10 + '0');
 				}
 				else if (a >= 10)
 				{
 					_putchar(' ');
-					_putchar(' ');
 					_putchar(a / 10 + '0');
 					_putchar(a % 10 + '0');
-					if (j != n)
-						_putchar(',');
 				}
 				else
 				{
@@ -39,12 +34,11 @@ void print_times_table(int n)
 					{
 						_putchar(' ');
 						_putchar(' ');
-						_putchar(' ');
 					}
 					_putchar(a + '0');
-					if (j != n)
-						_putchar(',');
 				}
+				if (j != n)
+					_putchar(',');
 			}
 			_putchar('\n');
 		}
