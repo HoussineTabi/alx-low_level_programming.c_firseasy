4bf5f546bd2aac533c6b9ff3b711059117cd681f
elhoussien <0620036304he@gmail.com>
2023-03-26 09:05:17 -0700

    101task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print the some of al number that are multiple of 3 and 5
+ *
+ * Return: 0 end of main
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 ==0) || (i % 5 == 0))
+			sum+ = i;
+	}
+	printf("%d\n", sum);
+	return 0;
+}
