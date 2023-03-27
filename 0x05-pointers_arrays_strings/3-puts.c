a9d19e21d8ce99b6e8420cf42b098097801a8769
elhoussien <0620036304he@gmail.com>
2023-03-27 04:55:22 -0700

    putsring

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "mian.h"
+
+/**
+ * _puts - this function print a string in the output
+ * @str: character parameter
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+		_putchar(*str);
+	_putchar('\n');
+}
